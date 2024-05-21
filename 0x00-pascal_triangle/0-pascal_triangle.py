2c82aefca16e6d7ededb77b337b43c32ba2375c6
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-05-21 17:49:19 +0000

    Created: the functional function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,3 +7,16 @@ return - A list of lists that contains the pascal's triangle.
 def pascal_triangle(n):
     if n <= 0:
         return []
+    triangle = []
+    for row_num in range(n):
+        row = [1]
+        if triangle:
+            prev = triangle[-1]
+            for num in range(1, len(prev)):
+                row.append(prev[num - 1] + prev[num])
+            row.append(1)
+            triangle.append(row)
+        else:
+            triangle.append([1])
+            
+    return triangle
---
+++ README.md
@@ -0,0 +1,2 @@
+# ALX-INTERVIEW
+This repository contains all the directories on the alx interview prospect questions.
